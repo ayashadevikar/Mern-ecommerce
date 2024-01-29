c101e0efbe244ed8603d12d6ad7ec36621efd9eb
ayashadevikar <ayashadevikar8@gmail.com>
2024-01-29 18:47:54 +0530

    cart added

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import CartPage from './pages/CartPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/" element={<Home />}/>
                 <Route path="/login" element={<LoginPage  />}/>
                 <Route path="/signup" element={<SignUpPage />}/>
+                <Route path="/cart" element={<CartPage />}/>
             </Routes>
       </Router>
        
