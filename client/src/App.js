11a32795ffd400e9dd27ef824be7ce9479dc219b
ayashadevikar <ayashadevikar8@gmail.com>
2024-01-30 18:21:34 +0530

    checkout page added

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
+import ProductDetailPage from './pages/ProductDetailPage'
 
 function App() {
   return (
@@ -16,7 +18,9 @@ function App() {
                 <Route path="/" element={<Home />}/>
                 <Route path="/login" element={<LoginPage  />}/>
                 <Route path="/signup" element={<SignUpPage />}/>
-                <Route path="/cart" element={<CartPage />}/>
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/Checkout" element={<Checkout />} />
+                <Route path="/product-detail" element={<ProductDetailPage />} />
             </Routes>
       </Router>
        
